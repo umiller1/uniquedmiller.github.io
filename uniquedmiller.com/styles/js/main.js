afd082840b9f2088e18de6f1726ee657009bb6f3
Unique M <uniquedmiller@gmail.com>
2024-01-20 19:19:28 -0500

    Coming Soon!

---
+++ uniquedmiller.com/styles/js/main.js
@@ -0,0 +1,12 @@
+let hamburger = document.querySelector('#hamburger');
+let menu = document.querySelector('#mainnav');
+let anchors = document.querySelector('#mainnav ul');
+
+hamburger.addEventListener('click', function() {
+  // hamburger.classList.toggle('isactive');
+  menu.classList.toggle('mobstyle');
+});
+anchors.addEventListener('click', function() {
+  // hamburger.classList.toggle('isactive');
+  menu.classList.remove('mobstyle');
+});
