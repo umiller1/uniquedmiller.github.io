aac0bec826460d57a90f03341fac3a266a935da9
Unique M <uniquedmiller@gmail.com>
2024-06-17 13:36:55 -0400

    e

--- alt/styles/js/project-nav.js
+++ alt/styles/js/project-nav.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-  let listbtn = '#p-menu li';
-  
-  $(listbtn).on('click', function() {
-      if (!$(this).hasClass('active')) {
-          $(this).addClass('active');
-      }
+  // Event listener using event delegation for all li elements under #p-menu
+  $('#p-menu').on("click", "li", function() {
+    // Remove 'active' class from all li elements
+    $('#p-menu li').removeClass('active');
+
+    // Add 'active' class to the clicked li element
+    $(this).addClass('active');
+
+    // Toggle corresponding accordion section
+    var id = $(this).attr('id').slice(0, -3); // Extract id without 'btn'
+    $('.accord').removeClass('active');
+    $('#' + id + 'p').addClass('active');
   });
 });
 // // navigation
@@ -17,13 +23,21 @@ $(document).ready(function() {
 // let listbtn = document.querySelector('#p-menu li');
 
 // $(document).ready(function() {
-//   $(listbtn).on('click', function() {
-//       if (!$(this).hasClass('active')) {
-//           $(this).addClass('active');
-//       }
+//   // Event listener for element with ID "ub"
+//   $(ub).on("click", function() {
+//     if ($(this).hasClass("active")) {
+//       $(this).removeClass("active");
+//     } else {
+//       $(this).addClass("active");
+//     }
 //   });
-// });
-// anchors.addEventListener('click', function() {
-//   // hamburger.classList.toggle('isactive');
-//   menu.classList.remove('mobstyle');
+
+//   // Event listener for element with ID "eb"
+//   $(eb).on("click", function() {
+//     if ($(this).hasClass("active")) {
+//       $(this).removeClass("active");
+//     } else {
+//       $(this).addClass("active");
+//     }
+//   });
 // });
